5418f397b7d95164c5d721cd621551fa6717d06f
yinruei <yinruei@g.ncu.edu.tw>
2019-11-14 19:51:54 +0800

    modify path

--- rd/data/get_data.py
+++ rd/data/get_data.py
@@ -12,12 +12,13 @@ from django.conf import settings
 
 def get_green_restaurant_data():
     green_restaurant = []
-    df = pd.read_excel("data.xlsx", sheet_name="A綠餐廳蔬食")
+    df = pd.read_excel(os.path.join(settings.DATA_ROOT, "map_data.xlsx"), sheet_name="A綠餐廳蔬食")
     header = ['店名', '地址(A-格式不限)', '經度', '緯度', '電話', '開放時間(統一格式)',
             '照片0__(fb目前頭貼)', '照片1','照片2', '照片3', '照片4', '照片5', '照片6',
             '照片(推薦頁)','近期更新時間']
 
-    data = df.replace(np.nan, '', regex=True)
+    filter_header_data = df[header]
+    data = filter_header_data.replace(np.nan, '', regex=True)
 
     img_ch_list = ['照片0__(fb目前頭貼)', '照片1', '照片2', '照片3', '照片4', '照片5', '照片6', '照片(推薦頁)']
     for i, row in enumerate(data.values):
@@ -49,6 +50,14 @@ def get_green_restaurant_data():
 def get_reed_datas():
     df = pd.read_excel(os.path.join(settings.DATA_ROOT, "plants.xlsx"))
     fetched_reed_datas = df.loc[df['vernacularName'].isin(['臺灣蘆竹', '蘆竹', '蘆葦', '臺灣蘆葦'])]
-    print(fetched_reed_datas)
+    # print(len(fetched_reed_datas))
+    header = ['decimalLongitude','decimalLatitude', 'vernacularName']
+    filter_header_data = fetched_reed_datas[header]
+    # print(filter_header_data)
 
-# get_reed_datas()
+    # for i, row in enumerate(data.values):
+    #     data_index = data.loc[i]
+    #     print(data_index)
+
+
+get_reed_datas()
