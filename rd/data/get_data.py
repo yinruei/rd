6b1ef8d905bdc8745f71ce20ee0aaf2df523b77b
yinruei <yinruei@g.ncu.edu.tw>
2019-11-14 20:17:43 +0800

    fix lat lon bugs

--- rd/data/get_data.py
+++ rd/data/get_data.py
@@ -22,6 +22,7 @@ def get_green_restaurant_data():
 
     img_ch_list = ['照片0__(fb目前頭貼)', '照片1', '照片2', '照片3', '照片4', '照片5', '照片6', '照片(推薦頁)']
     for i, row in enumerate(data.values):
+
         data_index = data.loc[i]
         data_dict= data_index.to_dict()
         data_dict['name'] = data_dict.pop('店名')
@@ -32,32 +33,42 @@ def get_green_restaurant_data():
         data_dict['bussiness_time'] = data_dict.pop('開放時間(統一格式)')
         data_dict['updtime'] = data_dict.pop('近期更新時間')
         data_dict['updtime'] = str(data_dict['updtime'])[:6]
+        # if data_dict['lon'] == "" or data_dict['lat'] == "":
+        #     print(i)
+        #     del green_restaurant[i]
+        # return
+
         img_list = []
         data_dict['imgs'] = img_list
-
         for key, value in data_dict.items():
             if key in img_ch_list:
                 if value != "":
                     img_list.append(value)
 
+
+
+
         del data_dict['照片0__(fb目前頭貼)'], data_dict['照片1'], data_dict['照片2'], data_dict['照片3'], data_dict['照片4'], data_dict['照片5'], data_dict['照片6'], data_dict['照片(推薦頁)']
         green_restaurant.append(data_dict)
+        if data_dict['lon'] == "" or data_dict['lat'] == "":
+            del green_restaurant[i]
 
     return green_restaurant
 
-# get_green_restaurant_data()
+get_green_restaurant_data()
 
 def get_reed_datas():
     df = pd.read_excel(os.path.join(settings.DATA_ROOT, "plants.xlsx"))
-    fetched_reed_datas = df.loc[df['vernacularName'].isin(['臺灣蘆竹', '蘆竹', '蘆葦', '臺灣蘆葦'])]
+    fetched_reed_datas = df.loc[df['vernacularName'].isin(['臺灣蘆竹', '蘆竹', '蘆葦', '臺灣蘆葦', '開卡蘆'])]
     # print(len(fetched_reed_datas))
     header = ['decimalLongitude','decimalLatitude', 'vernacularName']
     filter_header_data = fetched_reed_datas[header]
     # print(filter_header_data)
+    print(type(filter_header_data))
 
-    # for i, row in enumerate(data.values):
-    #     data_index = data.loc[i]
-    #     print(data_index)
+    for index, row in filter_header_data.iterrows():
+        print(index) # 输出列名
+        data_index = data.loc[index]
+        print(data_index)
 
-
-get_reed_datas()
+# get_reed_datas()
